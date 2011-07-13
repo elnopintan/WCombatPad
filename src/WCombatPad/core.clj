7f401b30701061de180449eb987efa208879b3d2
Ignacio Blasco <elnopintan@gmail.com>
2011-07-14 01:00:35 +0200

    rendering the mat

--- src/WCombatPad/core.clj
+++ src/WCombatPad/core.clj
@@ -12,7 +12,13 @@
 (defn filter-loged [{{loged :loged :as session} :session uri :uri } fn & params]
   (if loged (apply fn params)
       (assoc (redirect "/login") :session (assoc session :redirection uri))))
-(defn show-combat [combat-name] combat-name)
+(defn get-combat-data [combat-name] "")
+(defn show-mat [combat-data] [:section#mat ""])
+(defn show-characters [combat-data] [:nav#characters ""])
+(defn show-actions [combat-data] [:nav#actions ""])
+
+(defn show-combat [combat-name] (let [combat-data (get-combat-data combat-name)](html5 (map #(% combat-data) [show-mat show-characters show-actions]))))
 (defn link-to-combat [combat-name] [:a {:href (str "/combat/" combat-name)} combat-name])
+
 (defn show-list [] 
-  (html5 (unordered-list [(link-to-combat "uno") "dos" "tres"])))
+  (html5 (unordered-list (map link-to-combat ["uno" "dos" "tres"]))))
