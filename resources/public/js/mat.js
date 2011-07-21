b10312ff3f31c459108a4b10f75b9e5eaed1750b
Ignacio Blasco <elnopintan@gmail.com>
2011-07-22 00:19:26 +0200

    Begin to create List

--- resources/public/js/mat.js
+++ resources/public/js/mat.js
@@ -1,2 +1,7 @@
 $(function(){
+    $(document).mousemove(function(e){
+        var aMap=$("#map");
+	$("#position").css("visibility",false);
+       // $("#position").css("top",e.pageY+gridSize-(e.pageY%gridSize)-offsetY).css("left",e.pageX-(e.pageX%gridSize)-offsetX);
+               });
     $(".token").draggable({grid: [gridSize,gridSize]});});
--- src/WCombatPad/core.clj
+++ src/WCombatPad/core.clj
@@ -17,14 +17,15 @@
 (defn filter-loged [{{loged :loged :as session} :session uri :uri } fn & params]
   (if loged (apply fn params)
       (assoc (redirect "/login") :session (assoc session :redirection uri))))
-(defn get-map-headers [{grid-size :grid-size}] [:head (include-css "/css/mat.css")
-          [:script {:type "text/javascript"} (str "gridSize=" grid-size)]
+(defn get-map-headers [{grid-size :grid-size [offset-x offset-y] :offset }] [:head (include-css "/css/mat.css")
+          [:script {:type "text/javascript"} (str "gridSize=" grid-size"; offsetX=" offset-x "; offsetY=" offset-y";"  )]
           (include-js 
             "/js/jquery-1.6.1.min.js" 
             "/js/jquery-ui-1.8.12.custom.min.js"
             "/js/mat.js")])
-(defn get-combat-data [combat-name]
-  {:name combat-name
+(defn get-combat-data [combat-name] {:name "" :avatar "" :offset [0 0] :grid-size 0 } )
+(def ejemplo
+  {:name "ejemplo"
    :mat "almacen_hundido_bajo_parcial"
    :grid-size 21
    :offset [8 18]
@@ -46,7 +47,7 @@
    [:div#position {:style (str "width:" (- grid-size 4) "px;"
                                "height:" (- grid-size 4) "px;"
                                ) } ""]
-   [:img#map {:src (str "/combat/" combat-name "/map/" mat) :style (str "left:-" (* grid-size (+ 1(count characters))) "px;") }]
+   [:img#map {:src (str "/combat/" combat-name "/map/" mat) :style (str "left:-" (* grid-size (count characters)) "px;") }]
    ])
 (defn show-character [{char-name :name image :avatar}]
   [:div.character [:img {:src image :width "30px" :height "30px" }] char-name])
@@ -56,16 +57,20 @@
                                       
 (defn show-actions [combat-data] [:section#actions
                                   (unordered-list ["move" "move" "move"])])
+(defn paint-grid [graphics width height offset-x offset-y grid-size ]
+  (do   (.setColor graphics Color/BLACK)
+        (doall (map #(.drawLine graphics 0 % width %)
+             (take-while #(< % height) (iterate #(+ grid-size %) offset-y))))
+        (doall (map #(.drawLine graphics % 0 % height)
+                    (take-while #(< % width) (iterate #(+ grid-size %) offset-x))))))
+
 (defn get-map [map-name ]
   (let [{grid-size :grid-size [offset-x offset-y] :offset} (get-combat-data map-name)
         image (ImageIO/read (URL. (str "http://localhost:3000/images/maps/" map-name ".jpg")))
         output-stream (ByteArrayOutputStream.)
         graphics (.createGraphics image)]
-    (do (.setColor graphics Color/BLACK)
-        (doall (map #(.drawLine graphics 0 % (.getWidth image) %)
-             (take-while #(< % (.getHeight image)) (iterate #(+ grid-size %) offset-y))))
-        (doall (map #(.drawLine graphics % 0 % (.getHeight image))
-             (take-while #(< % (.getWidth image)) (iterate #(+ grid-size %) offset-x))))
+    (do
+        (paint-grid graphics (.getWidth image) (.getHeight image) offset-x offset-y grid-size)
         (ImageIO/write image "png" output-stream)
         (ByteArrayInputStream. (.toByteArray output-stream)))))
 
@@ -75,6 +80,9 @@
            (show-mat combat-data) 
            [:nav (map #(% combat-data) [show-characters show-actions])])))
 (defn link-to-combat [combat-name] [:a {:href (str "/combat/" combat-name)} combat-name])
-
+(defn get-pad-list [] [])
 (defn show-list [] 
-  (html5 (unordered-list (map link-to-combat ["uno" "dos" "tres"]))))
+  (html5 (unordered-list
+          (conj (map link-to-combat (get-pad-list))
+          "nuevo tablero" )
+          )))
