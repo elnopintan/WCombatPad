d4c51536b307954bd7fcef669889b9a19c579b6b
Ignacio Blasco <elnopintan@gmail.com>
2011-08-21 22:35:20 +0200

    first migration2

---
+++ src/WCombatPad/migration.clj
@@ -0,0 +1,14 @@
+(ns WCombatPad.migration
+  (:use [somnium.congomongo])
+  (:use [WCombatPad.data :only (get-combat-data
+                                )])
+  )
+
+(defn set_default_sizes []
+  (do
+    (map
+     (fn [data]
+       (update! :combat-status data
+                (assoc data :characters
+                       (map #(assoc % :size 1) (data :characters)))))
+     (fetch :combat-status))))
